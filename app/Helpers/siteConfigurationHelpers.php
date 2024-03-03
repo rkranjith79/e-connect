d912022d7d3ff3707e112adf3ac511121c43fdd8
rkranjith79 <Rkranjith79.rk@gmail.com>
2024-03-03 23:43:50 +0530

    Mrudhul: Conflict issue fixed

--- app/Helpers/siteConfigurationHelpers.php
+++ app/Helpers/siteConfigurationHelpers.php
@@ -1,21 +1,21 @@
 <?php
 
 function __getSiteConfigration($key, $return_key = "value") {
-    
+
    $siteConfigration = config('siteconfigrations')[$key] ?? [];
 
     if($return_key == "value") {
-        if(\Illuminate\Support\Facades\App::currentLocale() == 'ta' && optional($siteConfigration['attributes'])?->language_tamil_value ?? null) {
-            return optional($siteConfigration['attributes'])?->language_tamil_value;
+        if(\Illuminate\Support\Facades\App::currentLocale() == 'ta' && optional($siteConfigration['attributes'] ?? [])?->language_tamil_value ?? null) {
+            return optional($siteConfigration['attributes'] ?? [])?->language_tamil_value;
         }
-        return optional($siteConfigration['attributes'])?->value;
+        return optional($siteConfigration['attributes'] ?? [])?->value;
     } else if($return_key == "label") {
-        if(\Illuminate\Support\Facades\App::currentLocale() == 'ta' && optional($siteConfigration['attributes'])?->language_tamil_label ?? null) {
-          
-            return optional($siteConfigration['attributes'])?->language_tamil_label;
+        if(\Illuminate\Support\Facades\App::currentLocale() == 'ta' && optional($siteConfigration['attributes'] ?? [])?->language_tamil_label ?? null) {
+
+            return optional($siteConfigration['attributes'] ?? [])?->language_tamil_label;
         }
-        return $siteConfigration['label'];
+        return $siteConfigration['label'] ?? null;
     }
-    
-    return optional($siteConfigration['attributes'])?->$return_key ?? '';
+
+    return optional($siteConfigration['attributes'] ?? [])?->$return_key ?? '';
 }
