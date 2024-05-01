e3a129b919b6cf19d3b224219e22cd3215f94af6
rkranjith79 <Rkranjith79.rk@gmail.com>
2024-05-01 19:31:17 +0530

    racke:plan seeder added

---
+++ database/seeders/PlanSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Plan;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PlanSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Plan::create([
+            "title" => "Basic Plan",
+            "expire_in_days" => 365,
+            "attributes" => [],
+            "profile_count" => 1,
+            "order_by" => 1,
+            "price" => 40
+        ]);
+    }
+}
