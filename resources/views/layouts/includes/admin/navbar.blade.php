58ecd4dcf332ac4945a4ba540161d6237032aa26
rkranjith <rkranjith79.rk@gmail.com>
2024-07-07 13:27:58 +0530

    nav logo - parthiban

--- resources/views/layouts/includes/admin/navbar.blade.php
+++ resources/views/layouts/includes/admin/navbar.blade.php
@@ -124,7 +124,7 @@
             </li> --}}
             <li class="nav-item nav-profile dropdown">
                 <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
-                    <img loading="lazy" src="{{asset('admin/images/faces/face5.jpg')}}" alt="profile" />
+                    <img loading="lazy" src="{{asset('img/logo-e-connet.png')}}" alt="profile" />
                     <span class="nav-profile-name">{{ Auth::user()->name ?? '' }}</span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
